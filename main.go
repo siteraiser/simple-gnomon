7a868cc70dade728a5d87d71935ddf4732bd13f2
Carl Turechek <carl@siteraiser.com>
2025-12-28 18:04:30 -0500

    More comfortable settings

--- main.go
+++ main.go
@@ -27,9 +27,9 @@ var UseMem = true                    // Use in-memory db
 var SpamLevel = 50
 
 // Optimized settings for mode db mode
-var memBatchSize = int16(100)
-var memPreferredRequests = uint8(20)
-var diskBatchSize = int16(25)
+var memBatchSize = int16(128)
+var memPreferredRequests = uint8(16)
+var diskBatchSize = int16(64)
 var diskPreferredRequests = uint8(10)
 
 // Program vars
